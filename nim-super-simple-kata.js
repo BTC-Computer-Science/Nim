5b4b7c8b8a297eae161ed2c084faac485d632cef
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-01-12 22:55:56 -0500

    Create nim-super-simple-kata.js

---
+++ nim-super-simple-kata.js
@@ -0,0 +1,27 @@
+/* Nim Super Simple */
+
+/* Globals 
+ * count = integer, winner = String
+ */ 
+ 
+ /* Main Code 
+  * loop while count <21
+  * if count < 21 call cpuTurn, adding to count and alerting count
+  * if count < 21 call playerTurn, adding to count and alerting count
+  * end loop
+  * alert winner
+  */
+  
+/* playerTurn
+ * set winner as CPU
+ * prompt for turn (1-3)
+ * @param none
+ * @return turn (integer)
+ */
+
+/* cpuTurn
+ * set winner as player
+ * set turn as random number 1-3
+ * alert turn
+ * return turn
+ */
