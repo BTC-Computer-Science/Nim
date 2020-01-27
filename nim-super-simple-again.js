822f5af4ed36f08b9b20068c1aee347050a949df
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-01-27 16:46:46 -0500

    Create nim-super-simple-again.js

---
+++ nim-super-simple-again.js
@@ -0,0 +1,34 @@
+/* Nim Super Simple with Validation */
+// Use this flowchart: https://www.lucidchart.com/invitations/accept/0e16f178-84df-4b0d-924a-46d942fb72ee
+
+/* Start
+ * count = 0, turn = 0; winner = null, again = true;
+ * loop while again = true
+ * Call main code
+ * Ask if play again
+ */ 
+ 
+ /* Main Code 
+  * loop while count <21
+  * if count < 21 call cpuTurn, adding to count and alerting count. Set winner as player. 
+  * if count < 21 call playerTurn, adding to count and alerting count. Set winner as CPU.
+  * end loop
+  * alert winner
+  * @param none
+  * @return none
+  */
+  
+/* playerTurn
+ * set winner as CPU
+ * prompt for turn as integer (1-3) and allow only 1,2 or 3 as turns
+ * @param none
+ * @return turn 
+ */
+
+/* cpuTurn
+ * set winner as player
+ * set turn as random number 1-3, but count must not go over 21
+ * alert CPU's turn return turn
+ * @param none
+ * @return turn
+ */
