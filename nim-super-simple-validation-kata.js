7eaa3768bf93c25d98d09f2c839d0c42f1610ee1
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-01-27 16:40:11 -0500

    Create nim-super-simple-validation-kata.js

---
+++ nim-super-simple-validation-kata.js
@@ -0,0 +1,31 @@
+/* Nim Super Simple with Validation */
+// Use this flowchart: https://www.lucidchart.com/invitations/accept/0e16f178-84df-4b0d-924a-46d942fb72ee
+
+/* Globals 
+ * count = 0, turn = 0; winner = null
+ */ 
+ 
+ /* Main Code 
+  * loop while count <21
+  * if count < 21 call cpuTurn, adding to count and alerting count. Set winner as player. 
+  * if count < 21 call playerTurn, adding to count and alerting count. Set winner as CPU.
+  * end loop
+  * alert winner
+  * @param none
+  * @return none
+  */
+  
+/* playerTurn
+ * set winner as CPU
+ * prompt for turn as integer (1-3) and allow only 1,2 or 3 as turns
+ * @param none
+ * @return turn 
+ */
+
+/* cpuTurn
+ * set winner as player
+ * set turn as random number 1-3, but count must not go over 21
+ * alert CPU's turn return turn
+ * @param none
+ * @return turn
+ */
