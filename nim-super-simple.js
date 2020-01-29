f4b3b9f5ce1bf7d47d0bf4bc10b52f01c32b838d
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-01-29 14:30:11 -0500

    Update nim-super-simple.js

--- nim-super-simple.js
+++ nim-super-simple.js
@@ -1,16 +1,20 @@
 // Nim Super Duper Simple (no input validation)
 var count = 0; var turn = 0; var winner=null;
-while (count<21) {
-   winner="CPU"; 
-   count+=playerTurn();
-   alert("Current count is "+count);
-   if (count<21) {
-      winner="you"; 
-      count+=cpuTurn();
-      alert("Current count is "+count);
-   }
+main(); 
+alert(winner+" won!"); 
+
+function main() {	
+	while (count<21) {
+	   winner="CPU"; 
+	   count+=playerTurn();
+	   alert("Current count is "+count);
+	   if (count<21) {
+		  winner="you"; 
+		  count+=cpuTurn();
+		  alert("Current count is "+count);
+	   }
+	}
 }
-alert(winner+" won!");
 
 function playerTurn(){
   var turn = prompt("Count how many? (1-3)");
